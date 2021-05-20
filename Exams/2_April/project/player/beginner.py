7b86e6e77717a81cac60c98bf4403d7d1ebfb8f3
Arksiana <arksiana@gmail.com>
2021-05-20 16:48:49 +0300

    02/04

---
+++ Exams/2_April/project/player/beginner.py
@@ -0,0 +1,9 @@
+from project.player.player import Player
+
+
+class Beginner(Player):
+    initial_health_points = 50
+
+    def __init__(self, username: str):
+        super().__init__(username, self.initial_health_points)
+
