5e0155be94ac4e3b8756ef33058fd87f09d73b14
Arksiana <arksiana@gmail.com>
2021-05-25 12:30:44 +0300

    update_tests

--- Exams/2_April/tests/test_battlefield.py
+++ Exams/2_April/tests/test_battlefield.py
@@ -17,5 +17,39 @@ class TestBattle(unittest.TestCase):
         self.assertEqual(90, attacker.health)
         self.assertEqual(250, enemy.health)
 
+
+    def test_enemy_is_beginner_should_increase_health_points(self):
+        attacker = Advanced('attacker')
+        enemy = Beginner('enemy')
+
+        self.battle_field.fight(attacker, enemy)
+        self.assertEqual(250, attacker.health)
+        self.assertEqual(90, enemy.health)
+
+    def test_attacker_is_dead_should_raise_exception(self):
+        attacker = Beginner('attacker')
+        enemy = Advanced('enemy')
+
+        attacker.health = 0
+
+        with self.assertRaises(ValueError) as context:
+            self.battle_field.fight(attacker, enemy)
+
+        self.assertEqual(str(context.exception), 'Player is dead!')
+
+    def test_enemy_is_dead_should_raise_exception(self):
+        attacker = Advanced('attacker')
+        enemy = Advanced('enemy')
+
+        enemy.health = 0
+
+        with self.assertRaises(ValueError) as context:
+            self.battle_field.fight(attacker, enemy)
+
+        self.assertEqual(str(context.exception), 'Player is dead!')
+
+
+
+
 if __name__ == '__main__':
     unittest.main()
